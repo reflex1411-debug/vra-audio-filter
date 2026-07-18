10a77eceae53996155ff688195eefb08e2246a72
reflex1411-debug <reflex1411@gmail.com>
2026-07-18 13:03:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -122,10 +122,10 @@ if uploaded_file is not None:
         uploaded_file.seek(0)
         
         # Create a visually clean download bar for each track
-        st.download_button(
+       st.download_button(
             label=f"📥 Download {label} Track",
             data=processed_buffer,
-            file_name=f"{base_name}_{band_suffix}_NBN.wav",
+            file_name=f"{base_name}_{band_suffix}.wav",
             mime="audio/wav",
             key=label
         )
